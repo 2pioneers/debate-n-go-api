6a6b7399ca67ef5355c4b71ce39c522c6f7bc0b7
Justin Walrath <walrathjaw@gmail.com>
2014-01-18 14:04:01 -0600

    Checkin to move to laptop for unit testing.

--- main/controller/LoginController.php
+++ main/controller/LoginController.php
@@ -1,4 +1,4 @@
-<?php namespace Main\Database\Database;
+<?php namespace Main\Controller;
 
 /**
  * Manages the login controls.
@@ -19,17 +19,54 @@ class LoginController {
 	 * Constructs the controller for use.
 	 */
 	public function __construct($uniqueLoginUrl) {
+		$this->userDao = new \Main\Database\UserDao();
 		$this->uniqueLoginUrl = $uniqueLoginUrl;
-		$this->checkSessionForUser();
 	}
 	
 	/**
+	 * Tries to log in via the user.
 	 * 
+	 * @return string JSON encoded string with either a data-filled response or an error response.
 	 */
-	private function checkSessionForUser() {
-		if(isset($_SESSION['userData'])) {
+	public function attemptLogin() {
+		$returnJsonArray = array();
+		$userData = $this->searchUserData();
+		if(!is_null($userData)) {
+			$returnJsonArray = array();
+			$returnJsonArray["userData"] = $userData;
+			//TODO: logic to establish associated voting options.
+			
+			//Quickly setup the session for future use.
+			$this->refreshSession();
 			
 		}
+		else {
+			$returnJsonArray = $this->createErrorArray("User was not found. Do you have the wrong url?");
+		}
+		
+		return json_encode($returnJsonArray);
+	}
+	
+	/**
+	 * Searches the database for the user based on the url.
+	 * 
+	 * @return null|UserData The user data if present.
+	 */
+	private function searchUserData() {
+		return $this->userDao->searchUserByUrlExtension($this->uniqueLoginUrl);
+	}
+	
+	/**
+	 * Refreshes the session variable with passed in data.
+	 * 
+	 * @param array $newSessionData The data to be loaded into session.
+	 */
+	private function refreshSession($newSessionData) {
+		$lifetime = 0;
+		session_set_cookie_params($lifetime);
+		session_start();
+		
+		$_SESSION["userData"] = $newSessionData["userData"];
 	}
 }
 
---
+++ test/controller/LoginControllerTest.php
@@ -0,0 +1,10 @@
+<?php namespace Test\Controller;
+
+/**
+ * Tests the login processes.
+ */
+class LoginControllerTest extends \PHPUnit_Framework_TestCase {
+	
+}
+
+?>
