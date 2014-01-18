c0aa87fb77ee8930704d5ea0f238fb8d4d7a8238
Justin Walrath <walrathjaw@gmail.com>
2014-01-18 17:08:14 -0600

    need to move to a mongodb capable server.

--- main/database/VotingTopicDao.php
+++ main/database/VotingTopicDao.php
@@ -11,12 +11,20 @@ class VotingTopicDao {
 	private $coreDao;
 	
 	/**
-	 * Constructs the user dao
+	 * Constructs the user dao.
 	 */
 	public function __construct() {
 		$this->coreDao = CoreDao::getInstance();
 	}
 	
+	/**
+	 * Looks up the topic by the user id.
+	 * 
+	 * @param MongoId $userId User id to lookup by. 
+	 */
+	public function lookupTopicViaUserId($userId) {
+		//$this->coreDao->getVoting_topics()->
+	}
 }
 
 ?>
