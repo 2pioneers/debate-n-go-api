ac1787e1ce5011e74fb2be69a174a13187744339
Justin Walrath <walrathjaw@gmail.com>
2014-01-18 16:51:31 -0600

    Added a voting topics TO.

---
+++ main/database/VotingTopicDao.php
@@ -0,0 +1,22 @@
+<?php namespace Main\Database;
+
+/**
+ * Voting topic data functions.
+ */
+class VotingTopicDao {
+	
+	/**
+	 * Core Dao.
+	 */
+	private $coreDao;
+	
+	/**
+	 * Constructs the user dao
+	 */
+	public function __construct() {
+		$this->coreDao = CoreDao::getInstance();
+	}
+	
+}
+
+?>
---
+++ main/to/VotingTopicData.php
@@ -0,0 +1,131 @@
+<?php namespace Main\To;
+
+/**
+ * Container object for the voting topic data.
+ */
+class VotingTopicData {
+
+	/**
+	 * The voting topic id.
+	 */
+	private $id;
+	
+	/**
+	 * The description of the voting topic.
+	 */
+	private $description;
+	
+	/**
+	 * The status of the voting topic.
+	 */
+	private $status;
+	
+	/**
+	 * The list of options associated with this object.
+	 */
+	private $options;
+	
+	/**
+	 * The list of users associated with this topic.
+	 */
+	private $users;
+	
+	/**
+	 * Constructs the object with data.
+	 * 
+	 * @param MongoId $id The voting topic id.
+	 * @param string $description The description of the voting topic.
+	 * @param string $status The status of the object.
+	 * @param array $options The options allowed for the user to choose.
+	 * @param array $users The users that are associated with the voting topic.
+	 */
+	public function __construct($id, $description, $status, $options, $users) {
+		$this->id = $id;
+		$this->description = $description;
+		$this->status = $status;
+		$this->options = $options;
+		$this->users = $users;
+	}
+	
+	/**
+	 * Gets the id.
+	 * @return MongoId The id.
+	 */
+	public function getId() {
+		return $this->id;
+	}
+	
+	/**
+	 * Sets the id.
+	 * @param MongoId $id the id to set. 
+	 */
+	public function setId($id) {
+		$this->id = $id;
+	}
+	
+	
+	/**
+	 * Gets the description.
+	 * @return string The description.
+	 */
+	public function getDescription() {
+		return $this->description;
+	}
+	
+	/**
+	 * Sets the description.
+	 * @param string $description the description to set. 
+	 */
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+	/**
+	 * Gets the status.
+	 * @return string The status.
+	 */
+	public function getStatus() {
+		return $this->status;
+	}
+	
+	/**
+	 * Sets the status.
+	 * @param string $status the status to set. 
+	 */
+	public function setStatus($status) {
+		$this->status = $status;
+	}
+	
+	/**
+	 * Gets the options.
+	 * @return array The options.
+	 */
+	public function getOptions() {
+		return $this->options;
+	}
+	
+	/**
+	 * Sets the options.
+	 * @param MongoId $options the options to set. 
+	 */
+	public function setOptions($options) {
+		$this->options = $options;
+	}
+	
+	/**
+	 * Gets the users.
+	 * @return array The users.
+	 */
+	public function getUsers() {
+		return $this->users;
+	}
+	
+	/**
+	 * Sets the users.
+	 * @param array $users the users to set. 
+	 */
+	public function setUsers($users) {
+		$this->users = $users;
+	}
+}
+
+?>
