90a402c3cd0388d7d93c9c71ca6a518c185d594d
Justin Walrath <walrathjaw@gmail.com>
2014-01-19 12:06:56 -0600

    Added slim to route simple login query.

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 require 'vendor/autoload.php';
 
+$liftime = 0;
+session_set_cookie_params($lifetime);
+session_start();
+
+$app = new \Slim\Slim();
+		
+$app->get('/login/:uniqueurl', function($uniqueUrl) {
+	$loginController = new \Main\Controller\LoginController($uniqueUrl);
+	$app->response()->header("Content-Type", "application/json");
+	echo($loginController->attemptLogin());
+});
+
+$app->run();
 
 ?>
