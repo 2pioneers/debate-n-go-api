84e6dfe54d596f565550c3cb96a317ac823bb996
Justin Walrath <walrathjaw@gmail.com>
2014-01-29 22:04:52 -0600

    converted update nickname to POST method.

--- index.php
+++ index.php
@@ -17,16 +17,26 @@ $app->get('/login/:uniqueurl', function($uniqueUrl) use($app) {
 	echo($response);
 });
 
-$app->post('/updateUsername/:newUsername', function($newUsername) use($app) {
-	$userInformationController = new Main\Controller\UserInformationController();
-	$response = $userInformationController->updateUsername($newUsername);
+$app->post('/updateUsername/', function() use($app) {
+	$body = $app->request()->getBody();
+	$response = null;
+	if(isset($body['user_id']) && isset($body['new_username'])) {
+		$userId = new \MongoId($body['user_id']);
+		$newUsername = $body['new_username'];
+		
+		$userInformationController = new Main\Controller\UserInformationController();
+		$response = $userInformationController->updateUsername($userId, $newUsername);
+	}
+	else {
+		$response = json_encode(array('status' => '400', 'message' => "Missing input data."));
+	}
 	$app->response()->header("Content-Type", "application/json");
 	echo($response);
 });
 
 $app->post('/userVote', function() use($app) {
 	$body = $app->request()->getBody();
-	//Should be getting {"userId": "asjhfjasglakhjsdlghasgkjas", "optionId": "asjlghasfklghflgkjfgkls"}
+	//Should be getting {"user_id": "asjhfjasglakhjsdlghasgkjas", "option_id": "asjlghasfklghflgkjfgkls"}
 	$app->response()->header("Content-Type", "application/json");
 	echo("");
 	//Will be returning basically log in information.
@@ -34,14 +44,14 @@ $app->post('/userVote', function() use($app) {
 
 $app->post('/leaveComment', function() use($app) {
 	$body = $app->request()->getBody();
-	//Should be getting {"userId": "shlashgaklj", "message":"hi ho diddly", "vote_options": ["id1, id2, id3"]}
+	//Should be getting {"user_id": "shlashgaklj", "message":"hi ho diddly", "vote_options": ["id1, id2, id3"]}
 	$app->response()->header("Content-Type", "application/json");
 	echo("");
 });
 
 $app->post('/leaveReply', function() use($app) {
 	$body = $app->request()->getBody();
-	//Should be getting {"userId": "shlashgaklj", "message":"hi ho diddly", "parent_id": "jskjhalshjg"}
+	//Should be getting {"user_id": "shlashgaklj", "message":"hi ho diddly", "parent_id": "jskjhalshjg"}
 	$app->response()->header("Content-Type", "application/json");
 	echo("");
 });
